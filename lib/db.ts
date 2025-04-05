230014b6da776070f918ecad9f3d229296f15bf8
av3me <avi@deltagen.ai>
2025-04-05 17:35:04 -0400

    Update Prisma configuration to use Neon adapter

--- lib/db.ts
+++ lib/db.ts
@@ -5,14 +5,11 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-const prismaClientSingleton = () => {
-  return new PrismaClient({ adapter })
-}
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db 
